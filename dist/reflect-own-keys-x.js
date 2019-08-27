2776a915b88dc6deaf0467cf78e08bb783296fe4
Graham Fairweather <xotic750@gmail.com>
2019-08-27 23:15:23 +0200

    Maintenance

--- dist/reflect-own-keys-x.js
+++ dist/reflect-own-keys-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-08-20T20:23:23.107Z",
+  "date": "2019-08-27T21:15:15.064Z",
   "describe": "",
   "description": "Sham for Reflect.ownKeys",
   "file": "reflect-own-keys-x.js",
